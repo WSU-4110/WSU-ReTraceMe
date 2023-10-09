90d6b65d4d13c8f8b37c14e00258848a59fa806a
Will Gan <cp0601@wayne.edu>
2023-10-09 15:04:39 -0400

    retrieve/store location data

---
+++ locate.sql
@@ -0,0 +1,13 @@
+-- CREATE DATABASE locater;
+/*
+database already created
+Database set as a skeleton for the waypoints
+CREATE TABLE loc (
+    id int auto_increment PRIMARY KEY,
+    Longitude varchar(50),
+    Latitude varchar(50),
+    timestamp datetime
+);
+*/
+use locater;
+select * from waypoints;
