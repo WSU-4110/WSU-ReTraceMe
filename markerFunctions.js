2c141b525eb7fce3724fda131c5453af27e0f763
Tristan Schaub <gk9089@wayne.edu>
2023-10-19 12:41:25 -0400

    Add comments

--- markerFunctions.js
+++ markerFunctions.js
@@ -1,13 +1,19 @@
-var markers = new Map();
+var markers = new Map(); // initialize hashmap
 var lastClickedMarker = null;
 
+//Place marker on map
 function placeMarker(lngLat) {
+    //Create new instance of marker
     var newMarker = new tt.Marker().setLngLat(lngLat).addTo(map)
+
+    //Insert marker into hashmap
     markers.set(createKey(newMarker), newMarker)
 
+    //Assign popup to marker
     var popup = new tt.Popup({ offset: 25 }).setText('Lng: ' + lngLat.lng + '      Lat: ' + lngLat.lat);
     newMarker.setPopup(popup)
 
+    //Marker listens for click
     newMarker.getElement().addEventListener('click', function () {
         lastClickedMarker = newMarker
     });
@@ -15,6 +21,7 @@ function placeMarker(lngLat) {
     console.log("Marker placed at " + lngLat)
 }
 
+//User places marker
 function userPlaceMarker() {
     map.once('click', function (event) {
         var lngLat = new tt.LngLat(event.lngLat.lng, event.lngLat.lat)
@@ -22,12 +29,14 @@ function userPlaceMarker() {
     })
 }
 
+//Generate key for hash map
 function createKey(newMarker){
     var coords = newMarker.getLngLat()
     var key = coords.lng + " " + coords.lat;
     return key
 }
 
+//Remove last clicked marker
 function removeMarker() {
     if(lastClickedMarker){
         var key = createKey(lastClickedMarker);
@@ -41,6 +50,7 @@ function removeMarker() {
     }
 }
 
+//Remove every marker
 function removeAllMarkers() {
     for (let [key, marker] of markers) {
             marker.remove();
