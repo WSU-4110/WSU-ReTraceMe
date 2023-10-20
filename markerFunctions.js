31deac8bddf607175b1273872a1339b6817db245
Tristan Schaub <gk9089@wayne.edu>
2023-10-19 23:23:41 -0400

    Place markers at current location, add prototype for automatically placed waypoints

--- markerFunctions.js
+++ markerFunctions.js
@@ -2,31 +2,32 @@ var markers = new Map(); // initialize hashmap
 var lastClickedMarker = null;
 
 //Place marker on map
-function placeMarker(lngLat) {
-    //Create new instance of marker
-    var newMarker = new tt.Marker().setLngLat(lngLat).addTo(map)
+function placeMarker() {
+    navigator.geolocation.getCurrentPosition((position) => {
+        var lngLat = new tt.LngLat(position.coords.longitude, position.coords.latitude);
 
-    //Insert marker into hashmap
-    markers.set(createKey(newMarker), newMarker)
+        //Create new instance of marker
+        var newMarker = new tt.Marker().setLngLat(lngLat).addTo(map)
 
-    //Assign popup to marker
-    var popup = new tt.Popup({ offset: 25 }).setText('Lng: ' + lngLat.lng + '      Lat: ' + lngLat.lat);
-    newMarker.setPopup(popup)
+        //Insert marker into hashmap
+        markers.set(createKey(newMarker), newMarker)
 
-    //Marker listens for click
-    newMarker.getElement().addEventListener('click', function () {
-        lastClickedMarker = newMarker
+        //Assign popup to marker
+        var popup = new tt.Popup({ offset: 25 }).setText('Lng: ' + lngLat.lng + '      Lat: ' + lngLat.lat);
+        newMarker.setPopup(popup)
+
+        //Marker listens for click
+        newMarker.getElement().addEventListener('click', function () {
+            lastClickedMarker = newMarker
+        });
+
+        console.log("Marker placed at " + lngLat)
     });
-
-    console.log("Marker placed at " + lngLat)
 }
 
-//User places marker
-function userPlaceMarker() {
-    map.once('click', function (event) {
-        var lngLat = new tt.LngLat(event.lngLat.lng, event.lngLat.lat)
-        placeMarker(lngLat)
-    })
+function autoPlaceMarker() {
+    var interval = 300000; // 5 minutes
+    setInterval(placeMarker,interval)
 }
 
 //Generate key for hash map
