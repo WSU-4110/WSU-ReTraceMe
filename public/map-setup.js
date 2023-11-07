3bc6c12b10a7c3e20ee3a0dffb00960b19a43a89
Tristan Schaub <gk9089@wayne.edu>
2023-11-07 17:20:33 -0500

    Connect style switch to toggle button

--- public/map-setup.js
+++ public/map-setup.js
@@ -1,5 +1,6 @@
 var WSU = { lat: 42.357384, lng: -83.069739 };
 var center;
+let style = 'light';
 
 var map = tt.map({
     key: "wWiYy6VgRhpS4PPvp2RT0aDq7o7MTNAD",
@@ -10,18 +11,15 @@ var map = tt.map({
 });
 
 
-changeStyle() {
+function changeStyle() {
     if (style == 'light') {
         style = 'dark';
     }
     else {
         style = 'light';
     }
+    map.setStyle('https://api.tomtom.com/style/1/style/22.2.1-*/?map=2/basic_street-' + style + '&poi=2/poi_' + style);
 }
-
-
-map.setStyle('https://api.tomtom.com/style/1/style/22.2.1-*/?map=2/basic_street-' + style + '&poi=2/poi_' + style);
-
 
 const marker = new tt.Marker().setLngLat(WSU).addTo(map);
 const popup = new tt.Popup({ anchor: 'top' }).setText('Wayne State University Campus');
